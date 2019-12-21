7c3c7128e95ebc39bc6b208696f0a3fa6691b0a3
Donghua <shidonghua@meituan.com>
2019-12-21 12:29:48 +0800

    Initial Commit

---
+++ src/test/java/com/stone/studio/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.studio.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
